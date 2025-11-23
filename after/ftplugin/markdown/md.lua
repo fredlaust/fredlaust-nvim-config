2cba5d310ccf4c07a6c91ca3aa330769e91d814c
FLKN <frederik@norregaard.info>
2025-11-23 01:52:42 +0100

    filetype colorschemes

--- after/ftplugin/markdown/md.lua
+++ after/ftplugin/markdown/md.lua
@@ -8,3 +8,11 @@ vim.keymap.set('n', '<C-p>', function()
     setlocal signcolumn=yes:9
   ]]
 end, { desc = 'Enable wrapping and layout options' })
+
+-- https://www.reddit.com/r/neovim/comments/ynm6n6/comment/iva5pgg/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+-- Hentede den nedenstående funktionalitet fra reddit
+local colorscheme = 'flexoki2'
+local ok, _ = pcall(vim.api.nvim_command, 'colorscheme ' .. colorscheme)
+if not ok then
+    print 'error setting colorscheme'
+end
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,45 @@
+return {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        --
+        -- animate
+        -- bigfile = { enabled = true },
+        -- bufdelete = { enabled = true },
+        -- dashboard = { enabled = true },
+        -- debug = { enabled = true },
+        -- dim = { enabled = true },
+        -- explorer = { enabled = true },
+        -- gh = { enabled = true },
+        -- git = { enabled = true },
+        -- gitbrowse = { enabled = true },
+        image = { enabled = true },
+        -- indent = { enabled = true },
+        -- input = { enabled = true },
+        -- keymap = { enabled = true },
+        -- layout = { enabled = true },
+        lazygit = { enabled = true },
+        -- notifier = { enabled = true },
+        -- notify = { enabled = true },
+        -- picker = { enabled = true },
+        -- profiler = { enabled = true },
+        -- quickfile = { enabled = true },
+        -- rename = { enabled = true },
+        -- scope = { enabled = true },
+        -- scratch = { enabled = true },
+        -- scroll = { enabled = true },
+        -- statuscolumn = { enabled = true },
+        -- terminal = { enabled = true },
+        -- toggle = { enabled = true },
+        -- util = { enabled = true },
+        -- win = { enabled = true },
+        -- words = { enabled = true },
+        -- zen = { enabled = true },
+    },
+    keys = {},
+}
